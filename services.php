e7b0d565aa6f90b46a43b9a780cfab6655fcc5ae
Ansh Agarwal <57933984+anshagarwal012@users.noreply.github.com>
2023-01-25 11:47:04 +0530

    Service Page Done

--- services.php
+++ services.php
@@ -2,31 +2,61 @@
 $heading = "Our Services";
 require('header.php');
 ?>
-<div class="about-agency section-padding">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-2"></div>
-            <div class="col-md-8 col-sm-12">
-                <div class="agency-details">
-                    <h2>Our Services</h2>
-                    <p>
-                    <ul>
-                        <li>
-                            <p>Fundraising for pre-seed and seed rounds</p>
-                            <p>Acceleration program for companies at the early stages, providing guidance and support</p>
-                            <p>Ancillary Services -<br>
-                                i. &nbsp;&nbsp;Fund raise Documents - Deck and Financial Model<br>
-                                ii. &nbsp;Business Structuring & Advisory services<br>
-                                iii. Accounting & Secretarial services<br>
-                            </p>
-                        </li>
-                    </ul>
-                    </p>
+<div class="section-padding">
+    <section class="section services-section" id="services">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="section-title">
+                        <h2>Our Services</h2>
+                    </div>
                 </div>
             </div>
-            <div class="col-md-2"></div>
+            <div class="row">
+                <!-- feaure box -->
+                <div class="col-sm-6 col-lg-6">
+                    <div class="feature-box-1">
+                        <div class="icon">
+                            <i class="fa fa-money"></i>
+                        </div>
+                        <div class="feature-content">
+                            <h5>Fundraising</h5>
+                            <p>for pre-seed and seed rounds</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- / -->
+                <!-- feaure box -->
+                <div class="col-sm-6 col-lg-6">
+                    <div class="feature-box-1">
+                        <div class="icon">
+                            <i class="fa fa-tachometer"></i>
+                        </div>
+                        <div class="feature-content">
+                            <h5>Acceleration Program</h5>
+                            <p>for companies at the early stages, providing guidance and support.</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- / -->
+                <!-- feaure box -->
+                <div class="col-sm-12 col-lg-12">
+                    <div class="feature-box-1">
+                        <div class="icon">
+                            <i class="fa fa-rocket"></i>
+                        </div>
+                        <div class="feature-content">
+                            <h5>Ancillary Services</h5>
+                            <p>i. Fund raise Documents - Deck and Financial Model<br>
+                                ii. Business Structuring & Advisory services<br>
+                                iii. Accounting & Secretarial services</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- / -->
+            </div>
         </div>
-    </div>
+    </section>
 </div>
 <?php
 require('footer.php');
