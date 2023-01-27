e0f12efe705e03c9754911ab49684fa42bfaf219
Ansh Agarwal <57933984+anshagarwal012@users.noreply.github.com>
2023-01-27 18:20:09 +0530

    add line

--- services.php
+++ services.php
@@ -13,49 +13,13 @@ require('header.php');
                     </div>
                 </div>
             </div>
-            <!-- <div class="row">
-                <div class="col-sm-6 col-lg-6">
-                    <div class="feature-box-1">
-                        <div class="icon">
-                            <i class="fa fa-money"></i>
-                        </div>
-                        <div class="feature-content">
-                            <h5>Fundraising</h5>
-                            <p>for pre-seed and seed rounds</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-6 col-lg-6">
-                    <div class="feature-box-1">
-                        <div class="icon">
-                            <i class="fa fa-tachometer"></i>
-                        </div>
-                        <div class="feature-content">
-                            <h5>Acceleration Program</h5>
-                            <p>for companies at the early stages, providing guidance and support.</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-12 col-lg-12">
-                    <div class="feature-box-1">
-                        <div class="icon">
-                            <i class="fa fa-rocket"></i>
-                        </div>
-                        <div class="feature-content">
-                            <h5>Ancillary Services</h5>
-                            <p>i. Fund raise Documents - Deck and Financial Model<br>
-                                ii. Business Structuring & Advisory services<br>
-                                iii. Accounting & Secretarial services</p>
-                        </div>
-                    </div>
-                </div>
-            </div> -->
             <div class="row services justify-content-center mt-4">
                 <div class="col-sm-6 col-xl-3  my-auto p-3">
                     <div class="value single-team my-auto p-4">
                         <img src="/assets/svg/fundraising.svg" alt="" class="value-image mb-3">
                         <h4 class="text-center value-title">
                             Fundraising Support</h4>
+                            <hr>
                         <p class="text-center value-text">
                             Our fundraising support services aid startups in securing funding for their pre-seed and seed rounds. </p>
                     </div>
@@ -65,6 +29,7 @@ require('header.php');
                         <img src="/assets/svg/acceleration.svg" alt="" class="value-image">
                         <h4 class="text-center value-title">
                             Acceleration Support </h4>
+                            <hr>
                         <p class="text-center value-text">
                             Our acceleration program supports early-stage companies by offering guidance and assistance to help them grow and succeed. </p>
                     </div>
@@ -74,6 +39,7 @@ require('header.php');
                         <img src="/assets/svg/ancillary.svg" alt="" class="value-image">
                         <h4 class="text-center value-title">
                             Ancillary Services Support </h4>
+                            <hr>
                         <p class="text-center value-text">
                             i. Fund raise Documents - Deck and Financial Model<br>
                             ii. Business Structuring & Advisory services<br>
