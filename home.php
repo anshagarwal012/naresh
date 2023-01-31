9eabf86736cc1b320e11e7bcb8a92013e59bf413
Ansh Agarwal <57933984+anshagarwal012@users.noreply.github.com>
2023-01-31 13:02:33 +0530

    home color fix

--- home.php
+++ home.php
@@ -12,7 +12,7 @@
     </div>
   </div>
 </section>
-<div class="section-padding">
+<div class="section-padding bg-light">
   <div class="container">
     <div class="row">
       <div class="col-md-12">
