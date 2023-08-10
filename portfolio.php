d676548a94a56e496a44f1df3c7297312e54adff
Ansh Agarwal <info.ansh012@gmail.com>
2023-08-10 17:37:06 +0530

    Update portfolio.php

--- portfolio.php
+++ portfolio.php
@@ -19,7 +19,8 @@ require('header.php');
         display: flex;
         width: 100%;
         height: 100%;
-        padding-bottom: 100%;
+        /* padding-bottom: 100%; */
+        padding-bottom: 80%;
         flex-direction: column;
         justify-content: center;
         align-items: center;
@@ -36,6 +37,40 @@ require('header.php');
         position: absolute;
         bottom: 0;
     }
+
+    .portfolio-card.w-dyn-item {
+        border-radius: 0;
+        margin: 20px;
+        /* height: 75%;
+        width: 90%; */
+        border-top-right-radius: 30px;
+        border-bottom-left-radius: 30px;
+    }
+
+    .portfolio-card.w-dyn-item p {
+        margin: 0;
+        padding: 10px;
+        border-bottom-left-radius: 25px;
+        text-align: center;
+        color: #fff;
+        font-weight: bold;
+        /* width: 102%;
+        margin-left: -2px; */
+        /* scale: 1.03 */
+    }
+
+    /* @media (min-width : 800px) and (max-width : 1598px) {
+        .portfolio-card.w-dyn-item p {
+            scale: 1.02
+        }
+
+    } */
+
+    .row.justify-content-center {
+        /* width: 80%; */
+        margin: auto;
+        row-gap: 25px;
+    }
 </style>
 <div class="team-area bg-light">
     <div class="container section-padding">
@@ -51,6 +86,14 @@ require('header.php');
         </div>
         <div class="row justify-content-center">
             <?php
+            $color = [
+                'EV Mobility' => "#004d78",
+                'Enterprise IT' => "#4a8e6a",
+                'EdTech' => "#8e4a63",
+                'Fintech' => "#bc4b51",
+                'D2C' => "#8e4a4a",
+                'Deep Tech' => "#693c74",
+            ];
             $sql = "SELECT filename, cat FROM portfolio order by id desc";
             $result = mysqli_query($conn, $sql);
             $protocol = ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
@@ -58,10 +101,10 @@ require('header.php');
                 if (mysqli_num_rows($result) > 0) {
                     while ($row = mysqli_fetch_assoc($result)) { ?>
                         <div class="col-md-3 p-0">
-                            <div class="portfolio-card w-dyn-item">
+                            <div class="portfolio-card w-dyn-item" style="border: 3px solid <?= $color[$row['cat']] ?>;">
                                 <a style="background-image:url(&quot;<?= $protocol . $_SERVER['HTTP_HOST'] . '/admin/pf/' . $row['filename'] ?>&quot;)" href="javascript:void(0)" class="portfolio-card-link w-inline-block">
-                                    <p class="list"><?php echo $row['cat'] ?></p>
                                 </a>
+                                <p class="list" style="background-color: <?= $color[$row['cat']] ?>;"><?php echo $row['cat'] ?></p>
                             </div>
                         </div>
             <?php
